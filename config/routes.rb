03c4e0469246f5cd4bc213feabfd35b2ea2749c6
Veeral Patel <veeral.patel@fireeye.com>
2019-05-02 09:54:34 -0700

    redirect / to /systems

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :systems
   resources :engagements
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => redirect('/systems')
 end
